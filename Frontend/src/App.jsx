dbfe4171b15159451685b3851075e083ac4b0e18
Dhvanit <monparadhvanit@gmail.com>
2024-06-22 11:09:30 +0530

    added input comp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Input } from "./Components";
 
 function App() {
+    const [search, setSearch] = useState("");
 
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Hello world</h1>
+            <Input
+                label="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="search here"
+            />
+        </>
+    );
 }
 
-export default App
+export default App;
