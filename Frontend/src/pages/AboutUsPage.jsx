5e54f10efe3925b8f210abad9878ab5b6d7b1a07
Sahil-Gupta584 <134826929+Sahil-Gupta584@users.noreply.github.com>
2024-06-24 09:12:25 +0000

    updated contact and about page

--- Frontend/src/pages/AboutUsPage.jsx
+++ Frontend/src/pages/AboutUsPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import heroImg from "../media/about-img.jpeg";
 function AboutUsPage() {
   return (
-    <div>
-      About us page
+    <div className="hero flex items-baseline bg-transparent min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div>
+          <h3 className="text-5xl font-bold">About Our Healthcare Chatbot</h3>
+          <p className="py-6">
+            Welcome to our Healthcare Chatbot platform, your digital companion
+            in managing and navigating healthcare needs. Our chatbot is designed
+            to provide seamless assistance and personalized support for a
+            variety of healthcare-related inquiries, ensuring you get the
+            information and help you need efficiently and effortlessly.
+          </p>
+          <button className="btn bg-[#3fba97] text-black hover:bg-white hover:text-[#3fba97] border-2 border-[#3fba97]">
+            Get Started
+          </button>
+        </div>
+        <img src={heroImg} className="max-w-sm rounded-lg shadow-2xl" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default AboutUsPage
+export default AboutUsPage;
--- Frontend/src/pages/ContactPage.jsx
+++ Frontend/src/pages/ContactPage.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
 
 function ContactPage() {
   return (
-    <div>
-      Contact page
+    <div className="hero flex items-baseline bg-transparent min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Contact Us!</h1>
+          <p className="py-6">
+            We’re here to help you with any questions, concerns, or feedback you
+            may have about our Healthcare Chatbot. Your satisfaction and
+            well-being are our top priorities. Reach out to us through any of
+            the following methods, and our dedicated support team will be happy
+            to assist you.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactPage
+export default ContactPage;
